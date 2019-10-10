d869297d7525a5d46b3dbeece7b7a2a5dbb25b4c
bumbacoin <biscuit@192-168-1-101.tpgi.com.au>
2019-10-10 23:58:48 +0800

    fix naming issue

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/stronghands.png
-ICON_DST=../../src/qt/res/icons/stronghands.ico
-convert ${ICON_SRC} -resize 16x16 stronghands-16.png
-convert ${ICON_SRC} -resize 32x32 stronghands-32.png
-convert ${ICON_SRC} -resize 48x48 stronghands-48.png
-convert stronghands-48.png stronghands-32.png stronghands-16.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/Stronghands.png
+ICON_DST=../../src/qt/res/icons/Stronghands.ico
+convert ${ICON_SRC} -resize 16x16 Stronghands-16.png
+convert ${ICON_SRC} -resize 32x32 Stronghands-32.png
+convert ${ICON_SRC} -resize 48x48 Stronghands-48.png
+convert Stronghands-48.png Stronghands-32.png Stronghands-16.png ${ICON_DST}
 
